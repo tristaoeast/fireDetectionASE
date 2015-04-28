6efeeb852f26f25fc7f7c6598196506a4ed2f7f2
Tristao East <tmc@ASE-VirtualBox.(none)>
2015-04-28 11:14:33 +0100

    added some gps functionalities

---
+++ gps.h
@@ -0,0 +1,9 @@
+#ifndef GPS_H
+#define GPS_H
+
+typedef nx_struct position {
+	nx_uint16_t x;
+	nx_uint16_t y;
+} position;
+
+#endif
---
+++ gps.nc
@@ -0,0 +1,5 @@
+#include "gps.h"
+
+interface gps{
+	command position_t getPosition();
+}
